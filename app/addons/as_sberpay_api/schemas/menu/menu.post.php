f70d5ddcf20684b2b1ae98a0160d71e9b9b739c3
Father1993 <enjoyhillol@gmail.com>
2026-02-18 16:12:05 +1000

    fix: menu

--- app/addons/as_sberpay_api/schemas/menu/menu.post.php
+++ app/addons/as_sberpay_api/schemas/menu/menu.post.php
@@ -4,11 +4,16 @@ if (!defined('BOOTSTRAP')) {
     die('Access denied');
 }
 
-// Добавляем пункт меню в раздел "Аддоны"
 $schema['top']['addons']['items']['as_sberpay_api'] = [
-    'title' => 'SberPay API',
-    'href' => 'addons.update&addon=as_sberpay_api',
+    'title'    => 'SberPay API',
+    'href'     => 'addons.update&addon=as_sberpay_api',
     'position' => 100,
+];
+
+$schema['top']['addons']['items']['as_sberpay_api_payments'] = [
+    'title'    => 'SberPay API: Способы оплаты',
+    'href'     => 'payments.manage',
+    'position' => 101,
 ];
 
 return $schema;
