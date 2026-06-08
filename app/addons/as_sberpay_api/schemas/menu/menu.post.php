a56a30e11ab780f0688f4e9aef6906182f7d21f5
Father1993 <enjoyhillol@gmail.com>
2026-06-09 09:42:11 +1000

    Revert "feat(sberpay): add receipt audit feature with UI and DB support"
    This reverts commit ab953534108dd2d76209a9f29eb032333da6ab69.

--- app/addons/as_sberpay_api/schemas/menu/menu.post.php
+++ app/addons/as_sberpay_api/schemas/menu/menu.post.php
@@ -20,11 +20,6 @@ $schema['top']['addons']['items']['as_sberpay_api'] = [
             'href' => 'payments.manage',
             'position' => 20,
         ],
-        'receipt_audit' => [
-            'title' => 'Аудит чеков',
-            'href' => 'as_sberpay_api.receipt_audit',
-            'position' => 30,
-        ],
     ],
 ];
 
